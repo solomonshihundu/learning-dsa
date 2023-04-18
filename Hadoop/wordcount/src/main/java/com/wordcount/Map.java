3051c6ea4959ee992de2c70ec673680268549b20
solomonshihundu <shihundusolomon@gmail.com>
2023-04-18 12:49:35 +0300

    reduce class complete

--- Hadoop/wordcount/src/main/java/com/wordcount/Map.java
+++ Hadoop/wordcount/src/main/java/com/wordcount/Map.java
@@ -13,7 +13,7 @@ import org.apache.hadoop.mapreduce.Mapper;
  */
 import java.io.IOException;
 
-/* Map class extends the maaper class which has an implementation of
+/* Map class extends the mapper class which has an implementation of
     the map method
     1st arg : Input key is the line number of type Long
  * 2nd arg : Input value is of type text
@@ -26,11 +26,31 @@ public class Map extends Mapper<LongWritable, Text, Text, IntWritable>{
      * 
      * Called once for each key value input pair i.e line record
      * Key and Value parama corresponds to the 1st and 2nd args passed to the class
-     * The context endable the mapper to interract with rest of the hadoop system 
+     * The context endable the mapper to interract with rest of the hadoop system.
      */
+    @Override
     public void map(LongWritable key, Text value,Context context)
             throws IOException,InterruptedException{
-
+            
+            /*
+             * 
+             * Converst each input line to a string
+             * Create an array of the words separated by a space
+             * in the input string
+             */
+            String line = value.toString();
+            String[] words = line.split(" ");
+            
+            /*
+             * 
+             * Iterate through the input array this sets the output of the loop
+             * function as every occurance of every world word as the key together with a count of one
+             * as the value. 
+             */
+            for(String word : words){
+                context.write(new Text(word),new IntWritable(1));
             }
+
+        }
     
 }
--- Hadoop/wordcount/src/main/java/com/wordcount/Reduce.java
+++ Hadoop/wordcount/src/main/java/com/wordcount/Reduce.java
@@ -1,5 +1,48 @@
 package com.wordcount;
 
-public class Reduce {
+/*
+ * Import dependancies
+ */
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/*
+ * Mapper class throws IO exception
+ */
+import java.io.IOException;
+
+/*
+ * 
+ * The Reduce class extends the reducer class which and overrides the reduce method
+ * 1st and 2nd args: Inputs consist of a collection of keys and values pairs,common keys grouped together by shuffle operation
+ * 3rd and 4th atgs : Outputs consist of the unique input keys and there counts 
+ * 
+ */
+public class Reduce extends Reducer<Text,IntWritable,Text,LongWritable>{
+    /*
+     * 
+     * Called once for each distinct key from the shuffle phase
+     * Keys are the distinct words from input file
+     * values are the collection of ones whose length correspond to
+     * the number of occurance.
+     * Context enables us to write out the output
+     */
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+    throws IOException,InterruptedException{
+        int count=0;
+        /*
+         * Iterate through the collection of values
+         * increate the count variable with every iteration thus
+         * the final count would translate to the number of occurance of
+         * each word
+         */
+        for(IntWritable value : values){
+            count++;
+        }
+        context.write(key,new LongWritable(count));
+    }
     
 }
