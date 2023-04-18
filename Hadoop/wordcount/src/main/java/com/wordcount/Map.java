d551aaedfafd1046d7708c91b53dd6e276c62546
solomonshihundu <shihundusolomon@gmail.com>
2023-04-18 12:22:26 +0300

    hadoop project initial commit

---
+++ Hadoop/wordcount/src/main/java/com/wordcount/Map.java
@@ -0,0 +1,36 @@
+package com.wordcount;
+
+/*
+ * Import dependancies
+ */
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+/*
+ * Mapper class throws IO exception
+ */
+import java.io.IOException;
+
+/* Map class extends the maaper class which has an implementation of
+    the map method
+    1st arg : Input key is the line number of type Long
+ * 2nd arg : Input value is of type text
+ * 3rd and 4th args : Output is collection of key value pairs,i.e Key is each word in input 
+ * and value will be the occurance of the word always one
+ */
+public class Map extends Mapper<LongWritable, Text, Text, IntWritable>{
+
+    /*
+     * 
+     * Called once for each key value input pair i.e line record
+     * Key and Value parama corresponds to the 1st and 2nd args passed to the class
+     * The context endable the mapper to interract with rest of the hadoop system 
+     */
+    public void map(LongWritable key, Text value,Context context)
+            throws IOException,InterruptedException{
+
+            }
+    
+}
---
+++ Hadoop/wordcount/src/main/java/com/wordcount/Reduce.java
@@ -0,0 +1,5 @@
+package com.wordcount;
+
+public class Reduce {
+    
+}
